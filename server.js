69de247c4a435835ff55b544e1a72b68bf598377
Sylwia Modelewska <sylwiasupera@wp.pl>
2022-10-25 19:11:08 +0200

    Heroku problem

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /* global require, process */
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('build/db/app.json');
 const middlewares = jsonServer.defaults({
   static: 'build',
   noCors: true
 });
+const router = jsonServer.router('build/db/app.json');
 const port = process.env.PORT || 3131;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
